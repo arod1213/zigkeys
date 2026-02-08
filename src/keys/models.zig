a66361a43c5474252f229bf60cc922424d36798e
arod1213 <aidan@aidanrodriguez.com>
2026-02-08 14:27:00 -0800

    fix return

--- src/keys/models.zig
+++ src/keys/models.zig
@@ -53,7 +53,7 @@ pub const Key = struct {
     }
 
     pub fn equalPress(self: Self, other: Self) bool {
-        self.eq(other) and self.down == other.down;
+        return self.eq(other) and self.down == other.down;
     }
 
     pub fn eq(self: Self, other: Self) bool {
